1ef090602262c802776c99aa243cc7e2990f93d1
hieudt183739 <video06072000@gmail.com>
2021-03-21 17:29:16 +0700

    them

--- TTUD4/b.cpp
+++ TTUD4/b.cpp
@@ -1,3 +1,23 @@
+/*
+Một nhà thám hiểm cần đem theo một cái túi có trọng lượng không quá b. Có n đồ vật có thể đem theo. Đồ vật thứ j có trọng lượng aj và giá trị sử dụng cj. Hỏi nhà thám hiểm cần đem theo những đồ vật nào để cho tổng giá trị sử dụng là lớn nhất mà tổng trọng lượng đồ vật mang theo cái túi không vượt quá b?
+
+Input
+Dòng đầu tiên chứa hai số nguyên dương n,b (n≤30,b≤106).
+
+Dòng thứ j trong số n dòng tiếp theo mỗi dòng ghi ra hai số nguyên dương aj,cj≤106.
+
+Output
+Ghi ra duy nhất một số là tổng giá trị lớn nhất tìm được của các đồ vật cho vào túi.
+
+Example
+inputCopy
+2 6
+4 2
+3 2
+outputCopy
+2
+*/
+
 #include <iostream>
 using namespace std;
 
