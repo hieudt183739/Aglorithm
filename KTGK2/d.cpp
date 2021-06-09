af509e6a6e80107cb690d832bbea1d585b2b8621
hieudt183739 <video06072000@gmail.com>
2021-06-09 15:26:52 +0700

    them

--- KTGK2/d.cpp
+++ KTGK2/d.cpp
@@ -1,13 +1,36 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+const int N = 1010;
+int a[N];
+long long dp[N];
+vector<int> res;
+int n;
+
 int main(){
-    int a = 0;
-    int b = 1;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+    }
+    
+    dp[n + 1] = dp[n + 2] = 0;
+    for(int i = n; i >= 1; i--){
+        dp[i] = max(dp[i + 1], dp[i + 2] + a[i]);
+    }
+    int cur = 1;
+    while(cur <= n && dp[cur] != 0){
+        if(dp[cur] == dp[cur + 2] + a[cur]){
+            res.push_back(cur);
+            cur += 2;
+        }else cur += 1;
+    }
+    cout << dp[1] << endl;
+    cout << res.size() << endl;
+    for(int i = 0; i < res.size(); i++) cout << res[i] << " ";
+    cout << endl;
+    
     return 0;
 }
