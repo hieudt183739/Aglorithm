af42756c2609e1baeb66747269aed744c57cb1e4
hieudt183739 <video06072000@gmail.com>
2021-07-08 15:19:47 +0700

    them

--- KTGK3/b.cpp
+++ KTGK3/b.cpp
@@ -1,70 +1,71 @@
 #include<iostream>
-#include<utility>
+#include<algorithm>
 #include<vector>
 using namespace std;
 
-const int N = 100050;
-int n,q;
-vector<pair<int,int>> connected[N];
-int cut[N];
+const int N = 1e5 + 1;
+int n, q;
+pair<int, int> edge[N];
+int id[N];
+bool used[N];
+long long res[N];
 
-void dfs(int u, int j, int daddy,int &sum){
-    for(int i = 0; i < connected[u].size(); i++){
-        if(i == j) continue;
-        int v = connected[u][i].first;
-        if(v == daddy || v == 0) continue;
-        dfs(v,j,u,++sum);
+// DSU luu rung
+struct DSU{
+    int root[N]; // goc cua cay
+    int size[N]; // kich thuoc cua cay 
+    DSU(){
+        for(int i = 1; i <= n; i++){
+            root[i] = i;
+            size[i] = 1;
+        }
     }
-}
+    // tim goc cua 1 cay
+    int getRoot(int u){
+        if(root[u] == u) return u;
+        return root[u] = getRoot(root[u]);
+    }
+    // hop 2 cay 
+    void unite(int u, int v){
+        u = getRoot(u);
+        v = getRoot(v);
+        if(u == v) return;
+        root[u] = v;
+        size[v] += size[u];
+    }
+};
 
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(NULL);
-    
     cin >> n;
     for(int i = 1; i <= n - 1; i++){
-        int u,v;
+        int u, v;
         cin >> u >> v;
-        connected[u].push_back({v,i});
-        connected[v].push_back({u,i});
+        edge[i] = make_pair(u, v);
     }
     cin >> q;
     for(int i = 1; i <= q; i++){
-        cin >> cut[i];
-    }
-
-    for(int c = 1; c <= q; c++){
-        int i;
-        int j;
-        int v;
-        int k;
-        int flag = 0;
-        for(i = 1; i <= n; i++){
-            for(j = 0; j <= n; j++){
-                if(connected[i][j].second == cut[c]){
-                    v = connected[i][j].first;
-                    connected[i][j].first = 0;
-                 k = 0;
-                    while(1){
-                        if(connected[v][k].second == cut[c]){
-                            connected[v][k].first = 0;
-                            break;
-                        } 
-                        k++;
-                    }
-                    flag = 1;
-                    break;
-                }               
-            }
-            if (flag == 1) break;
+        cin >> id[i];
+        used[id[i]] = 1;
+    }    
+    
+    DSU dsu;
+    for(int i = 1; i <= n - 1; i++){
+        if(used[i] == 0){
+            dsu.unite(edge[i].first, edge[i].second);
         }
-        int sumX,sumY;
-        sumX = 1;
-        sumY = 1;
-        dfs(i,j,0,sumX);
-        dfs(v,k,0,sumY);
-        cout << sumX << " " << sumY << endl;
-
     }
+    for(int i = q; i; i--){
+        int u, v;
+        u = edge[id[i]].first;
+        v = edge[id[i]].second;
+        res[i] = 1ll * dsu.size[dsu.getRoot(u)] * dsu.size[dsu.getRoot(v)]; 
+        dsu.unite(u, v); // hop 2 cay lai voi nhau
+    }
+    for(int i = 1; i <= q; i++){
+        cout << res[i] << endl;
+    }
+    
+
     return 0;
 }
-
