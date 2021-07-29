38ca9709e9cfee64d6cb5a81211f54270e643626
hieudt183739 <video06072000@gmail.com>
2021-07-29 15:14:43 +0700

    them

---
+++ Hackerrank/Greedy/1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int const N = 120;
+int n, k;
+int c[N];
+int res = 0;
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++){
+        cin >> c[i];
+    }
+
+    sort(c + 1, c + n + 1);
+
+    int start = 0;
+    int dem = 0;
+
+    for(int i = n; i; i--){
+        res += c[i] * (start + 1);
+        dem++ ;
+        cout << res << endl;
+        if(dem % k == 0) start++ ;  
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
+
+
+
